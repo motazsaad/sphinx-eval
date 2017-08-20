82b88ae35842385435bc42a2a07eb928027050b7
Motaz Saad <motaz.saad@gmail.com>
2017-08-20 09:39:31 +0300

    generalize

---
+++ steps.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+bash sphinx-eval/test.sh dmps dicts acoustic test/kacst kacst hyp logs
--- test.sh
+++ test.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 7 ]; then
+    echo "usage ${0} lm_dmp_dir dic_dir acoustic_dir test_dir test_corpus hyp_dir log_dir";
+    exit -1;
+fi
+
 lm_dmp_dir=${1}
-hyp_dir=${2}
-dic_file=${3}
+dic_dir=${2}
+acoustic_dir=${3}
 test_dir=${4}
 test_corpus=${5}
-acoustic_dir=${6}
+hyp_dir=${6}
 log_dir={7}
+
+mkdir -p ${hyp_dir}
+mkdir -p ${log_dir}
 
 for lm in lm_dmp_dir
 do
 lm_name=$(basename $lm)
+dic_name=$(echo $lm_name | cut -d '.' -f1)
 nohup pocketsphinx_batch \
  -adcin yes \
  -cepdir ${test_dir}/${test_corpus}_wav \
  -cepext .wav \
  -ctl ${test_dir}/${test_corpus}_test.fileids \
  -lm ${lm} \
- -dict ${dict_file} \
+ -dict ${dic_dir}/${dic_name} \
  -hmm ${acoustic_dir}/model_parameters/s0192_spk.cd_cont_1000 \
- -hyp ${test_dir}/${hyp_dir}/${test_corpus}_${lm_name}.hyp &> ${log_dir}/${test_corpus}_${lm_name}.out&
+ -hyp ${test_dir}/${hyp_dir}/${test_corpus}_${lm_name}.hyp &> ${test_dir}/${log_dir}/${test_corpus}_${lm_name}.out&
 done 
+
 
