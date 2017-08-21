df5288fe0d41ed548946ea8a2a911cb3fa1a34bb
Motaz Saad <motaz.saad@gmail.com>
2017-08-21 22:45:38 +0200

    modify steps.sh and add new exp cmd

--- steps.sh
+++ steps.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 bash sphinx-eval/test.sh dmps dicts acoustic test/kacst kacst500 hyp logs
+
+
+bash sphinx-eval/test_3in1dic_gt100.sh dmps dicts acoustic test/kacst kacst500 hyp logs
+
+
