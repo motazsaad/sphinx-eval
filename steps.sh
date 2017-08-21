290fe92711bb5507200f9fca0c90f215e6869a1c
Motaz Saad <motaz.saad@gmail.com>
2017-08-21 22:52:09 +0200

    correct command

--- steps.sh
+++ steps.sh
@@ -3,6 +3,6 @@
 bash sphinx-eval/test.sh dmps dicts acoustic test/kacst kacst500 hyp logs
 
 
-bash sphinx-eval/test_3in1dic_gt100.sh dmps dicts acoustic test/kacst kacst500 hyp logs
+bash sphinx-eval/test3in1dic_gt100.sh dmps dicts acoustic test/kacst kacst500 hyp logs
 
 
