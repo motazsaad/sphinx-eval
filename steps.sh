145111afa226232e0abfa1c46c5986862a8c003d
Motaz Saad <motaz.saad@gmail.com>
2017-08-22 16:48:30 +0300

    word align cmds

--- steps.sh
+++ steps.sh
@@ -32,6 +32,19 @@ test/N7_020723_RMC/N7_020723_RMC_AR_test.transcription ${h} | grep 'TOTAL Percen
 done
 
 
+#########################################################
+
+# N7_040810_MED_AR
+bash sphinx-eval/test3in1dic_gt100.sh dmps dicts acoustic test/N7_040810_MED N7_040810_MED_AR \
+/home/msaad/speechdata/elra/sphinx_files/speech_test_wav hyp logs
+
+# evaluate N7_040810_MED_AR
+for h in  test/N7_040810_MED/hyp/*gt100*
+do
+echo ${h}
+perl /usr/local/lib/sphinxtrain/scripts/decode/word_align.pl \
+test/N7_040810_MED/N7_040810_MED_AR_test.transcription ${h} | grep 'TOTAL Percent'
+done
 
 
 
