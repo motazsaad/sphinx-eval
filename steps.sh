18daea6fe076b618bd3974b803bb9be6b4cf1eb5
Motaz Saad <motaz.saad@gmail.com>
2017-08-22 08:21:03 +0300

    collect wer in steps.sh

--- steps.sh
+++ steps.sh
@@ -5,4 +5,14 @@ bash sphinx-eval/test.sh dmps dicts acoustic test/kacst kacst500 hyp logs
 
 bash sphinx-eval/test3in1dic_gt100.sh dmps dicts acoustic test/kacst kacst500 hyp logs
 
+for h in test/kacst/hyp/*gt100*
+do
+echo ${h}
+perl /usr/local/lib/sphinxtrain/scripts/decode/word_align.pl \
+test/kacst/kacst500_test.transcription ${h} | grep 'TOTAL Percent'
+done
+
+
+
+
 
