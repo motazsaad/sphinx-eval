080768b4eb94044f34a17e92e90e188dd3547a0c
Motaz Saad <motaz.saad@gmail.com>
2017-08-26 23:23:04 +0300

    lm and ppl

--- collect_wer.sh
+++ collect_wer.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 
-test_dirs=$(ls -d asr-test/*"/")
+#test_dirs=$(ls -d asr-test/*"/")
+test_dirs=("asr-test/jsc/" "asr-test/kacst/" "asr-test/N7_020723_RMC/" "asr-test/N7_040810_MED/")
 for test_dir in ${test_dirs}
 do
-echo "" > ${test_dir}acc.all
-for f in ${test_dir}align/*
-do
-acc=$(cat ${f} | grep 'TOTAL Percent' | cut -d' ' -f11)
-printf "%s\t%s\n" ${acc} ${f} >> ${test_dir}acc.all
-done
-sort -n -r ${test_dir}acc.all > ${test_dir}acc_sorted.all
+    echo "" > ${test_dir}acc.all
+    for f in ${test_dir}align/*
+    do
+        acc=$(cat ${f} | grep 'TOTAL Percent' | cut -d' ' -f11)
+        printf "%s\t%s\n" ${acc} ${f} >> ${test_dir}acc.all
+    done
+    sort -n -r ${test_dir}acc.all > ${test_dir}acc_sorted.all
 done
 
 for test_dir in ${test_dirs}
 do
-cat  ${test_dir}acc_sorted.all
-echo "-----------------------------"
+    cat  ${test_dir}acc_sorted.all
+    echo "-----------------------------"
 done
 
