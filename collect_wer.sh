1dbc66ebb60314b244740459a907f14f27ff7979
Motaz Saad <motaz.saad@gmail.com>
2017-08-26 23:38:32 +0300

    lm and ppl

--- collect_wer.sh
+++ collect_wer.sh
@@ -12,10 +12,7 @@ do
         printf "%s\t%s\n" ${acc} ${f} >> ${test_dir}acc.all
     done
     sort -n -r ${test_dir}acc.all > ${test_dir}acc_sorted.all
-done
 
-for test_dir in ${test_dirs}
-do
     cat  ${test_dir}acc_sorted.all
     echo "-----------------------------"
 done
