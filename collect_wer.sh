eb42efa15205a22cc0878e6a7d8b96e8631963ff
Motaz Saad <motaz.saad@gmail.com>
2017-08-26 23:26:08 +0300

    lm and ppl

--- collect_wer.sh
+++ collect_wer.sh
@@ -2,8 +2,8 @@
 
 
 #test_dirs=$(ls -d asr-test/*"/")
-test_dirs=("asr-test/jsc/" "asr-test/kacst/" "asr-test/N7_020723_RMC/" "asr-test/N7_040810_MED/")
-for test_dir in "${test_dirs[@]}"
+
+for test_dir in "asr-test/jsc/" "asr-test/kacst/" "asr-test/N7_020723_RMC/" "asr-test/N7_040810_MED/"
 do
     echo "" > ${test_dir}acc.all
     for f in ${test_dir}align/*
