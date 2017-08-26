a665f0f54821108112266412bb50a121353fb9c5
Motaz Saad <motaz.saad@gmail.com>
2017-08-26 22:14:58 +0300

    lm and wer

--- collect_wer.sh
+++ collect_wer.sh
@@ -21,11 +21,23 @@ done
 echo "--------------------------"
 
 
-for h in hyp/kacst*.hyp
+
+
+test_dirs=$(ls -d asr-test/*"/")
+for test_dir in ${test_dirs}
 do
-echo ${h}
-perl /usr/local/lib/sphinxtrain/scripts/decode/word_align.pl \
-kacst2_test.transcription ${h} | grep 'TOTAL Percent'
+echo "" > ${test_dir}acc.all
+for f in ${test_dir}align/*
+do
+acc=$(cat ${f} | grep 'TOTAL Percent' | cut -d' ' -f11)
+printf "%s\t%s\n" ${acc} ${f} >> ${test_dir}acc.all
+done
+sort -n ${test_dir}acc.all > ${test_dir}acc_sorted.all
 done
 
+for test_dir in ${test_dirs}
+do
+cat  ${test_dir}acc_sorted.all
+echo "-----------------------------"
+done
 
