824824aca20f54614324cbad2147d1ee7aac24c8
Motaz Saad <motaz.saad@gmail.com>
2017-08-27 17:12:31 +0300

    fix path

--- steps_selected.sh
+++ steps_selected.sh
@@ -13,7 +13,7 @@ asr-test/kacst/kacst500_wav hyp logs
 
 # N7_020723_RMC_AR
 bash sphinx-eval/test3in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/N7_020723_RMC N7_020723_RMC_AR \
-/home/msaad/speechdata/elra/sphinx_files/speech_test_wav hyp logs
+/home/motaz/mod_home/speechdata/elra/sphinx_files/speech_test_wav hyp logs
 
 
 
@@ -21,7 +21,7 @@ bash sphinx-eval/test3in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/N7_020
 
 # N7_040810_MED_AR
 bash sphinx-eval/test3in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/N7_040810_MED N7_040810_MED_AR \
-/home/msaad/speechdata/elra/sphinx_files/speech_test_wav hyp logs
+/home/motaz/mod_home/speechdata/elra/sphinx_files/speech_test_wav hyp logs
 
 
 #########################################################
