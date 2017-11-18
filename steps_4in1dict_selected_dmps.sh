5f1a3c556ad431beb5b08c0e3079c8d609972682
Motaz Saad <motaz.saad@gmail.com>
2017-11-18 18:52:41 +0200

    add steps for 4in1 dic

---
+++ steps_4in1dict_selected_dmps.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+
+gt=200
+dmps_dir=selected_dmps
+
+# kacst
+bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/kacst kacst500 \
+asr-test/kacst/kacst500_wav hyp logs
+
+
+#########################################################
+
+# N7_020723_RMC_AR
+bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/N7_020723_RMC N7_020723_RMC_AR \
+/home/motaz/mod_home/speechdata/elra/sphinx_files/speech_test_wav hyp logs
+
+
+
+#########################################################
+
+# N7_040810_MED_AR
+bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/N7_040810_MED N7_040810_MED_AR \
+/home/motaz/mod_home/speechdata/elra/sphinx_files/speech_test_wav hyp logs
+
+
+#########################################################
+
+
+# jsc
+bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/jsc jsc \
+asr-test/jsc/jsc_wav hyp logs
