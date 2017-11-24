52b74223280e5c764bceaef7d1e16a564eb39466
Motaz Saad <motaz.saad@gmail.com>
2017-11-24 12:07:03 +0000

    develop oov count script

---
+++ oov_count.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+
+
+
+
+function get_oov_count {
+    test_dir = ${1}
+    mkdir -p ${test_dir}/oov_freq
+    for oov_file in ${test_dir}/oov/*.oov
+    do
+        oov_name=$(basename ${oov_file})
+        cat ${oov_file} | tr ' ' '\n' | sort | uniq -c > ${test_dir}/oov_freq/${oov_name}.freq
+    done
+}
+
+
+
+# oov count kacst
+test_corpus="asr-test/kacst"
+get_oov_count ${test_corpus}
+
+# oov count N7_020723_RMC_AR
+get_oov_count ${test_corpus}
+
+# oov count N7_040810_MED_AR
+test_corpus="asr-test/N7_040810_MED"
+get_oov_count ${test_corpus}
+
+# oov count jsc
+test_corpus="asr-test/jsc"
+get_oov_count ${test_corpus}
