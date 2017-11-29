96bcdd6aa30116ff2fc3f49badaa71b96a6e0374
Motaz Saad <motaz.saad@gmail.com>
2017-11-29 18:52:58 +0000

    fix mishkal command

--- get_diacritcs_from_mishkal.sh
+++ get_diacritcs_from_mishkal.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
 function get_diacritics {
-    diacritics_cmd=${1}    
-    input=${2}
-    output=${3}
+    input=${1}
+    output=${2}
     out_name=$(basename ${input})
     printf "processing %s\n" ${input}
-    ${diacritics_cmd} ${input} > ${output}/${out_name}
+    python mishkal/bin/mishkal-console.py -f ${input} > ${output}/${out_name}
 }
-
-mishkal_cmd="python mishkal/bin/mishkal-console.py -f"
 
 for oov_file in asr-test/oov_all/lines_*
 do
-    get_diacritics ${mishkal_cmd} ${oov_file} "asr-test/oov_all/mishkal"    
+    get_diacritics ${oov_file} "asr-test/oov_all/mishkal"    
 done
 
 printf "done\n"
