ddf90aa4dcc6d575eae0bdfba3c4dfa593e02e9f
Motaz Saad <motaz.saad@gmail.com>
2017-12-01 20:38:28 +0000

    fix dic name

--- steps_4in1dict_selected_dmps.sh
+++ steps_4in1dict_selected_dmps.sh
@@ -3,14 +3,14 @@
 
 gt=200
 dmps_dir=selected_dmps
-dic_name=4in1_gt1_mishkal_var5
-#dic_name=4in1_gt1_tashkeel_var5
-#dic_name=4in1_gt1_harakat_var5
+dic_name=4in1_gt1_mishkal_var7
+#dic_name=4in1_gt1_tashkeel_var7
+#dic_name=4in1_gt1_harakat_var7
+#dic_name=4in1_gt1_var5
 
 # kacst
 bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/kacst kacst500 \
 asr-test/kacst/kacst500_wav hyp logs ${dic_name}
-
 
 #########################################################
 
@@ -18,17 +18,13 @@ asr-test/kacst/kacst500_wav hyp logs ${dic_name}
 bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/N7_020723_RMC N7_020723_RMC_AR \
 /home/motaz/mod_home/speechdata/elra/sphinx_files/speech_test_wav hyp logs ${dic_name}
 
-
-
 #########################################################
 
 # N7_040810_MED_AR
 bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/N7_040810_MED N7_040810_MED_AR \
 /home/motaz/mod_home/speechdata/elra/sphinx_files/speech_test_wav hyp logs ${dic_name}
 
-
 #########################################################
-
 
 # jsc
 bash sphinx-eval/test4in1dic.sh ${dmps_dir} ${gt} dicts acoustic asr-test/jsc jsc \
